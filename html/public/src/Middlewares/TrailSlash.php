26dade712db4ba29f6948b571d4ff71292d97485
Chonlasith Jucksriporn <chonlasith@gmail.com>
2018-06-22 10:02:50 +0700

    Refactor middleware

---
+++ html/public/src/Middlewares/TrailSlash.php
@@ -0,0 +1,27 @@
+<?php
+namespace Qi\Middlewares;
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+class TrailSlash {
+    public function __invoke(Request $request, Response $response, callable $next)
+    {
+        $uri = $request->getUri();
+        $path = $uri->getPath();
+        if ($path != '/' && substr($path, -1) == '/') {
+            // permanently redirect paths with a trailing slash
+            // to their non-trailing counterpart
+            $uri = $uri->withPath(substr($path, 0, -1));
+            
+            if($request->getMethod() == 'GET') {
+                return $response->withRedirect((string)$uri, 301);
+            }
+            else {
+                return $next($request->withUri($uri), $response);
+            }
+        }
+    
+        return $next($request, $response);
+    }
+}
