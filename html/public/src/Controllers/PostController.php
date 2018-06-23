546138bd9f497e6e3c7dbad927f4dcf0f9eefe1f
Chonlasith Jucksriporn <chonlasith@gmail.com>
2018-06-23 23:44:06 +0700

    add some metadata

--- html/public/src/Controllers/PostController.php
+++ html/public/src/Controllers/PostController.php
@@ -20,12 +20,30 @@ class PostController {
             "page" => 1,
             "limit" => 10
         ], $req_data);
+        if ($pagination["page"] <= 0) {
+            $pagination["page"] = 1;
+        }
+        if ($pagination["limit"] <= 0) {
+            $pagination["limit"] = 10;
+        }
 
         $posts = Post::orderBy('id', 'asc')
             ->skip(($pagination["page"] - 1) * $pagination["limit"])
             ->take($pagination["limit"])
             ->get();
-        $r = $response->withJson($posts);
+        $count = Post::count();
+        $page_count = ceil($count / $pagination["limit"]);
+        if ($page_count === 0) {
+            $page_count = 1;
+        }
+
+        $result = [
+            "page_count" => $page_count,
+            "page" => $pagination["page"],
+            "limit" => $pagination["limit"],
+            "result" => $posts
+        ];
+        $r = $response->withJson($result);
         return $r;
     }
 
